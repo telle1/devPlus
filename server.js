9be023a1f4372f67cfb9145d5308e9263611e698
Tiffany Luu <tiffanyell1@gmail.com>
2020-12-06 14:53:59 -0800

    Users backend/model

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const connectDb = require('./config/db')
 
 const app = express();
+//Connect to database
+connectDb();
+//Init Middleware (Needed to get the data in the body request)
+app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('<h1>Hello World</h1>'))
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+app.post('/', (req,res) => res.send('hi'))
+
+// console.log(module)
 
 
 
